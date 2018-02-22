4f7e18b5f8b22d7746d1eaf71c6a138b752cbdb1
OlegFilth <obodin.oleg@gmail.com>
2018-02-22 22:22:26 +0300

    Way to best setup js

--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,19 @@
 'use strict';
-
-var setupElement = document.querySelector('.setup');
-setupElement.classList.remove('hidden');
-
-var setupSimilarList = document.querySelector('.setup-similar');
-setupSimilarList.classList.remove('hidden');
-
 var names = ['Иван', 'Хуан', 'Мария', 'Кристоф', 'Юлия', 'Виктор', 'Люпита', 'Вашингтон'];
 var lastNames = ['Да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Игвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161', 'rgb(56, 159, 117)','rgb(215, 210, 55', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+const NUMBER_OF_WIZARDS = 4;
+
+var showSetupBlock = (element) => {
+	element.classList.remove('hidden');
+};
+
+var setupElement = document.querySelector('.setup');
+var setupSimilarList = document.querySelector('.setup-similar');
+showSetupBlock(setupElement);
+showSetupBlock(setupSimilarList);
 
 var similarListElement = document.querySelector('.setup-similar-list');
 
@@ -19,17 +23,20 @@ var getRandomElement = (list) => {
 	return list[Math.floor(Math.random() * list.length)];
 };
 
-var getWizardElement = (count, template) => {
+var getWizardElement = (template) => {
+	var templateClone = template.cloneNode(true);
+
+	templateClone.querySelector('.setup-similar-label').textContent =  getRandomElement(names) + ' ' + getRandomElement(lastNames);
+	templateClone.querySelector('.wizard-coat').style.fill = getRandomElement(coatColors);
+	templateClone.querySelector('.wizard-eyes').style.fill = getRandomElement(eyesColors);
+
+	return templateClone;
+};
+
+var getWizardElements = (count) => {
 	var wizardsElements = [];
-
 	for (var i = 0; i < count; i++) {
-		var templateClone = template.cloneNode(true);
-
-		templateClone.querySelector('.setup-similar-label').textContent =  getRandomElement(names) + ' ' + getRandomElement(lastNames);
-		templateClone.querySelector('.wizard-coat').style.fill = getRandomElement(coatColors);
-		templateClone.querySelector('.wizard-eyes').style.fill = getRandomElement(eyesColors);
-
-		wizardsElements.push(templateClone);
+		wizardsElements.push(getWizardElement(similarWizardTemplate));
 	}
 	
 	return wizardsElements;
@@ -41,4 +48,4 @@ var renderFragment = (parentElement, elements) => {
 	parentElement.appendChild(fragment);	
 };
 
-renderFragment(similarListElement, getWizardElement(4,similarWizardTemplate));
+renderFragment(similarListElement, getWizardElements(NUMBER_OF_WIZARDS, getWizardElement(similarWizardTemplate)));
